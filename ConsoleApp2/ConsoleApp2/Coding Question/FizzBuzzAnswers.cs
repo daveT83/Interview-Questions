bc4c075aed1c6800df4200b68a210c0b44843a53
daveT83 <40746034+daveT83@users.noreply.github.com>
2024-09-13 12:36:16 -0400

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Coding Question/FizzBuzzAnswers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Coding_Question
+{
+    public class FizzBuzzAnswers
+    {
+        public void Solution1(int low, int high)
+        {
+            while (low <= high)
+            {
+                if (low % 3 == 0 && low % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (low % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (low % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                low++;
+            }
+        }
+        public void Solution2(int low, int high)
+        {
+            bool isNewLine = false;
+            while (low <= high)
+            {
+                if (low % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                    isNewLine = true;
+                }
+                if (low % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                    isNewLine = true;
+                }
+
+                if (isNewLine)
+                {
+                    Console.WriteLine();
+                    isNewLine = false;
+                }
+                low++;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Coding Question/FizzBuzzProblem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Coding_Question
+{
+    public class FizzBuzzProblem
+    {
+        /*
+         * Implement FizzBuzz
+         * iterate from low to high
+         * For example:
+         * low = 0
+         * high = 5
+         * iterate through 1, 2, 3, 4, 5
+         * 
+         * When a number is divisible by 3 print Fizz
+         * When a number is divisible by 5 print Buzz
+         * When a number is divisible by 3 and divisible by 5 print FizzBuzz
+         * When a number is not divisible by 3 or 5 do nothing.
+         * 
+         * When you are done and confident that it works, please walk us through it.
+         * 
+         * Bonus: What is the time Complexity of your solution? Please use Big O notation.
+         */
+        public void FizzBuzz(int low, int high)
+        {
+
+        }
+    }
+}
